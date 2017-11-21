7ac32949ccb6853c6c5ed367cb1e4cb037d15c43
Kevin Kraus <bartez94@hotmail.de>
2017-11-21 08:42:32 +0100

    create and init config and database and fill last one

---
+++ project/_config.py
@@ -0,0 +1,15 @@
+import os
+
+# grab the folder where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = "flasktaskr.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+
+# is used for cross-site request forgery prevention (app goes more secure)
+WTF_CSRF_ENABLED = True
+SECRET_KEY = "je|zUn@B6vYC4x&dl`_?uM|cEWp;'S"
+
+# define the full path for the DATABASE
+DATABASE_PATH = os.path.join(basedir, DATABASE)
---
+++ project/db_create.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+from _config import DATABASE_PATH
+
+with sqlite3.connect(DATABASE_PATH) as connection:
+    c = connection.cursor()
+    c.execute("""create table tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    name TEXT NOT NULL,
+                                    due_date TEXT NOT NULL,
+                                    priority INTEGER NOT NULL,
+                                    status INTEGER NOT NULL);""")
+    c.execute("""insert into tasks(name, due_date, priority, status)
+                    values('Finish this tutorial', '21/11/2017', 10, 1)""")
+    c.execute("""insert into tasks(name, due_date, priority, status)
+                    values('Finish Real Python Course 2', '21/11/2017', 10, 1);""")
