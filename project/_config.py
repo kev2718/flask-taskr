4d789ccc6a565c9becf913a4753770c07a226f38
Kevin Kraus <bartez94@hotmail.de>
2018-01-17 08:05:52 +0100

    ready for heroku pushing

--- project/_config.py
+++ project/_config.py
@@ -4,7 +4,7 @@ import os
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG=True
+DEBUG=False
 
 DATABASE = "flasktaskr.db"
 # is used for cross-site request forgery prevention (app goes more secure)
